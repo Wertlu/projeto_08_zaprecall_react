99359a2439389bdae35683eb014ca04168f431f9
Guga <contato.gustavobarbosa@outlook.com>
2021-12-20 09:45:53 -0300

    Preparando revisão de código

--- src/components/FrontFace.js
+++
@@ -1,9 +0,0 @@
-export default function FrontFace({question, img, setFace, cardPosition, deckLength}) {
-    return (
-        <div className="front-face">
-            <span data-identifier="counter">{`${cardPosition}/${deckLength}`}</span>
-            <h2>{question}</h2>
-            <img data-identifier="arrow" onClick={() => setFace("back")} classname="turn" src={img} alt="" />
-        </div>
-    );
-}
